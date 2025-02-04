a85899436ec466a07f00922cbfb1c97030807734
Shalaka <shalakakapure@gmail.com>
2025-02-04 10:17:17 +0530

    minor UI alignment fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
